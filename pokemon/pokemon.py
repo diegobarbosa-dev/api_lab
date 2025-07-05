1a0b548fbd0dbc7d19253cbe8920a6b42892b6d5
diegobarbosa <diegobarbosadev@gmail.com>
2025-07-05 07:51:56 -0300

    uso do json.loads

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -1,10 +1,24 @@
 import requests
+import json
+
 
 url = "https://pokeapi.co/api/v2/pokemon/"
 
 payload = {}
 headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = json.loads(requests.request("GET", 
+                            url, headers=headers, 
+                            data=payload).text)
 
-print(response.text)
+# A função json.loads():
+
+# Converte uma string JSON (vinda de .text)
+
+# Para um dicionário Python
+
+
+
+print(response["results"])
+
+
